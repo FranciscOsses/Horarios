c475c5a657fbe7f8549c57e54dd400b5dfd7f542
Francisco Osses <fosses43@gmail.com>
2022-01-19 13:00:13 -0300

    Atributos Empleados

--- src/main/java/com/example/horarios/Entidades/Empleado.java
+++ src/main/java/com/example/horarios/Entidades/Empleado.java
@@ -1,6 +1,14 @@
 
 package com.example.horarios.Entidades;
 
+import java.util.Date;
+
 public class Empleado {
     public long id;
+    public String nombre;
+    public Date diaFranco;
+    public int CargaHoraria;
+    public int HorasTrabajadas;
+    public int HorasExtras;
+    public boolean alta;
 }
